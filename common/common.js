c965a55bf340893d86d20d7c9928779622dc0c6c
Curtis Coureau <curtis.coureau@ynov.com>
2024-05-13 17:17:26 +0200

    amelioration de l'organisation du code et fix probleme de footer

--- common/common.js
+++ common/common.js
@@ -2,15 +2,24 @@ document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector('header');
     const footer = document.querySelector('footer');
 
-    fetch('common/header.html')
+    fetch('../common/header.html')
         .then(response => response.text())
         .then(data => {
             header.innerHTML = data;
         });
 
-    fetch('common/footer.html')
+    fetch('../common/footer.html')
         .then(response => response.text())
         .then(data => {
             footer.innerHTML = data;
         });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const nav = document.querySelector('nav');
+
+    burgerMenu.addEventListener('click', function () {
+        nav.classList.toggle('active');
+    });
 });
